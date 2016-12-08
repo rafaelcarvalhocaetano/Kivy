4eb8c759a4de04ed9ff8963989d9ebb52fb124ff
Rafael <rapha.pse@outlook.com>
2016-12-08 21:41:40 -0200

    Sistema de medidas

---
+++ Kivy_08_sistema_medida/source/main.py
@@ -0,0 +1,6 @@
+#pixel é a unidade de medida universal utilizada pelos fabricantes de hardware.
+#resolução de tela = é a quantidade de pixel do display
+#DPI = pontos por polegada; divisão da largura da tela pela resolução do display
+#DENSIDADE DA TELA = largura da tela em pixel / largura da tela em polegadas
+#DP = largura em pixel * 160 / densidade da tela
+#SP = escala independente de pixel
