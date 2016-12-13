75ca9168ffa287ca445be92930302b569c7bdc8d
Rafael <rapha.pse@outlook.com>
2016-12-13 15:32:39 -0200

    Posicionamento 2

---
+++ Kivy_11_posicionamento_2/source/posicionamento/main.py
@@ -0,0 +1,36 @@
+#right {right:1.}
+#top   {top:1.}
+#center {center:}
+"""
+ FloatLayout:
+   Button:
+       pos_hint:{"right: 1.}
+
+
+    Button:
+        pos_hint{'top':1.}
+
+
+
+-------posicionamento- direita-----
+FloatLayout:
+
+    Button:
+        right: 300
+        y: 0
+------------posicionamento top---------
+
+FloatLayout:
+
+    Button:
+        top: 300
+        y: 0
+------------posicionamento CENTER---------
+
+FloatLayout:
+
+    Button:
+        center_x:300
+        center_y:300
+
+"""
