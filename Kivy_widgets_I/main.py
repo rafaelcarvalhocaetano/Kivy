9db7700b7198c8a0e75de82d2fbafe7115dc46ee
Rafael <rapha.pse@outlook.com>
2016-12-22 20:47:23 -0200

    KIVY - Widget´s - I

---
+++ Kivy_widgets_I/main.py
@@ -0,0 +1,19 @@
+"""
+
+widgetBASE
+
+widget
+
+layout              label           image
+
+floatLayout         Button          asyncimage
+box                 toggleButton
+stack
+grid
+relative
+page
+scatter
+
+
+
+"""
