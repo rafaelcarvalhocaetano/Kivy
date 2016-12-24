3a6d3a949335470bd5ae0649cb5f3e0f10c8c07f
Rafael <rapha.pse@outlook.com>
2016-12-24 10:50:33 -0200

    Alterando a cor de Fundo

---
+++ Kivy_cor_de_fundo/source/main.py
@@ -0,0 +1,36 @@
+#coding: utf-8
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.utils import get_color_from_hex
+from kivy.core.window import Window
+
+#Window.clearcolor = [1, 1, 1, 1]
+Window.clearcolor = get_color_from_hex("#FFFFFF")
+
+kvcode = """
+#:import C kivy.utils.get_color_from_hex
+
+<FVerde@FloatLayout>:
+    size_hint: .3, .3
+
+    canvas.before:
+        Color:
+            rgba: C("#22FFAA")
+        Rectangle:
+            pos: self.pos
+            size: self.size
+FloatLayout:
+    FVerde:
+        pos_hint:{"x": .4, "y": .4}
+"""
+
+
+class JanelaApp(App):
+
+    def build(self):
+        return Builder.load_string( kvcode )
+
+janele = JanelaApp()
+janele.run()
