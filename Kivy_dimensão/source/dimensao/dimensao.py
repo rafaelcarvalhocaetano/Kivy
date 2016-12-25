b673b684b181c01c557412eff4d75c17279315e6
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-25 18:48:27 -0200

    Arrumando a identação

--- Kivy_dimensão/source/dimensao/dimensao.py
+++ Kivy_dimensão/source/dimensao/dimensao.py
@@ -5,22 +5,18 @@ Button:
 Button:
     sizze_hint_x: .4
     size_hint_y: .2
-
 -----------------------------
 Button:
     size_hint: (.8, .2)
 --------------------------------
-
 Button:
     size_hint_x: None
     width: 400
-
 --------------------------------
 Button:
     size_hint: (None, None)
     width: 400
     height: 200
-
 ------------------------------
 
 """
